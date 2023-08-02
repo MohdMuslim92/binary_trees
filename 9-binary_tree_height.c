2b89f3555895842e44c76e6f8221eaf5a447a14b
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-08-02 21:41:26 +0300

    Update 9-binary_tree_height.c
    - Add check to see if the tree has only a root node

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,6 +17,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
+	if (left_height == 0 && right_height == 0)
+		return (1);
 	if (left_height > right_height)
 		return (left_height + 1);
 	else
