7ea49aa0fac9edd1e34e276c1cc040ae0a6d55ca
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-08-02 14:53:36 +0300

    Update binary_trees.h
    - Edit header include guard

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,6 @@
-#ifndef __BINARY_TREES_H__
-#define __BINARY_TREES_H__
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
 
 #include <stddef.h>
 #include <stdio.h>
@@ -29,4 +30,4 @@ void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
-#endif /* __BINARY_TREES_H__ */
+#endif /* BINARY_TREES_H */
